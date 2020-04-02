b36640655de176404bf61fbbce15df7830c71bf3
Lucas Pallez <lucas.pallez@hotmail.com>
2020-04-02 10:31:29 +0200

    Initial Commit
    Defined main file structure and dependencies

---
+++ Src/main.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+#include "ch.h"
+#include "hal.h"
+#include "memory_protection.h"
+#include <main.h>
+#include <usbcfg.h>
+#include <motors.h>
+#include <audio/microphone.h>
+#include <chprintf.h>
+#include <arm_math.h>
+
+static void serial_start(void)
+{
+	static SerialConfig ser_cfg = {
+	    115200,
+	    0,
+	    0,
+	    0,
+	};
+
+	sdStart(&SD3, &ser_cfg); // UART3.
+}
+
+int main(void)
+{
+
+    halInit();
+    chSysInit();
+    mpu_init();
+
+    //starts the serial communication
+	serial_start();
+	//starts the USB communication
+	usb_start();
+	//inits the motors
+	motors_init();
+
+    /* Infinite loop. */
+    while (1) {
+    	//waits 1 second
+        chThdSleepMilliseconds(1000);
+    }
+}
+
+#define STACK_CHK_GUARD 0xe2dee396
+uintptr_t __stack_chk_guard = STACK_CHK_GUARD;
+
+void __stack_chk_fail(void)
+{
+    chSysHalt("Stack smashing detected");
+}
