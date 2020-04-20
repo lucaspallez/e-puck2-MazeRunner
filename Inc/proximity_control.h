e9be4eb51b8f31e9015bffc73fc2c15e85e7930f
Lucas Pallez <lucas.pallez@hotmail.com>
2020-04-20 21:41:06 +0200

    Started score calculations
    Added thread for measurements from the sides, not functional yet

--- Inc/proximity_control.h
+++ Inc/proximity_control.h
@@ -3,6 +3,8 @@
 
 #define IR_FRONT_RIGHT 	0		//IR1
 #define IR_FRONT_LEFT 	7		//IR8
+#define IR_LEFT			5		//IR6
+#define IR_RIGHT		2		//IR3
 #define NB_SENSOR		2		//Use of the 2 front sensors only
 #define PROX_THRESHOLD	1000 	//Empirically determined
 
--- Src/proximity_control.c
+++ Src/proximity_control.c
@@ -14,26 +14,55 @@ static THD_FUNCTION(wall_detection_thd, arg)
     while(1)
     {
     	time = chVTGetSystemTime();
-		uint16_t prox_average = 0;
+		uint16_t prox_front_average = 0;
+		uint16_t prox_side_average = 0;
+		uint16_t prox_left_corrected = 0;
+		uint16_t prox_right_corrected = 0;
+		uint16_t score_temp = 0;
 
-		prox_average =  get_calibrated_prox(IR_FRONT_LEFT) + get_calibrated_prox(IR_FRONT_RIGHT);	//Getting sensor data and taking the average
-		prox_average /= NB_SENSOR;
+		prox_front_average =  get_calibrated_prox(IR_FRONT_LEFT) + get_calibrated_prox(IR_FRONT_RIGHT);	//Getting sensor data and taking the average
+		prox_front_average /= NB_SENSOR;
 
-		if(prox_average >= PROX_THRESHOLD)
+		if(prox_front_average >= PROX_THRESHOLD) //If everything is alright
 		{
 			palClearPad(GPIOD, GPIOD_LED_FRONT);
 			palClearPad(GPIOB, GPIOB_LED_BODY);
 		}
-		else
+		else //If something is wrong (wall very close)
 		{
 			palSetPad(GPIOD, GPIOD_LED_FRONT);
 			palSetPad(GPIOB, GPIOB_LED_BODY);
 		}
+
+		prox_side_average =  get_calibrated_prox(IR_LEFT) + get_calibrated_prox(IR_RIGHT);	//Getting sensor data and taking the average
+		prox_side_average /= NB_SENSOR;
+		prox_left_corrected = get_calibrated_prox(IR_LEFT) - prox_side_average;
+
 		chThdSleepUntilWindowed(time, time + MS2ST(10)); //reduced the sample rate to 100Hz
     }
 }
 
+static THD_WORKING_AREA(score_thd_wa, 256);
+static THD_FUNCTION(score_thd, arg)
+{
+	(void) arg;
+    chRegSetThreadName(__FUNCTION__);
+
+    static uint32_t score;
+
+
+    while(1)
+    {
+
+    }
+
+}
 void wall_detection_start(void)
 {
 	chThdCreateStatic(wall_detection_thd_wa, sizeof(wall_detection_thd_wa), NORMALPRIO, wall_detection_thd, NULL);
+}
+
+void score_calculation_start(void)
+{
+	chThdCreateStatic(score_thd_wa, sizeof(score_thd_wa), NORMALPRIO, score_thd, NULL);
 }
