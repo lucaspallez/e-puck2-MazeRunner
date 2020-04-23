212575aefb7e90bfc6d67d032c7aaed5b79b82a8
Achraf <achraf.berrada@epfl.ch>
2020-04-23 09:13:47 +0000

    added stop and change direction

--- Src/motor_control.c
+++ Src/motor_control.c
@@ -23,9 +23,11 @@ void motor_control(float frequency) //test
 		}
 }
 
-void deplacment(float frequency) // dit au robot de tourner à droite, gauche ou de continuer tout
+void deplacement(float frequency) // dit au robot de tourner à droite, gauche ou de continuer tout
 // droit en fonction du son écouté.
 {
+	left_motor_set_pos(0);
+	right_motor_set_pos(0);
 	  //Violon:
 
 
@@ -35,12 +37,12 @@ void deplacment(float frequency) // dit au robot de tourner à droite, gauche ou 
 	int rotation; //1 si rotation en cours, 0 sinon
 
 	//Codage de la frequence écoutée en fonction du schéma //Violon:
-	if (f>250 & f<280) f=0; //Do: 265.625
-	if (f>280 & f<312) f=1; //Ré: 296.875
-	if (f>312 & f<358) f=2; //Mi: 328.125
-	if (f>358 & f<390) f=3; //Fa: 375
-	if (f>390 & f<430) f=4; //Sol: 406.25
-	if (f>430 & f<480) f=5; //La: 453.125
+	if (frequency>250 & frequency<280) f=0; //Do: 265.625
+	if (frequency>280 & frequency<312) f=1; //Ré: 296.875
+	if (frequency>312 & frequency<358) f=2; //Mi: 328.125
+	if (frequency>358 & frequency<390) f=3; //Fa: 375
+	if (frequency>390 & frequency<430) f=4; //Sol: 406.25
+	if (frequency>430 & frequency<480) f=5; //La: 453.125
 
 	// condition de déplacement
 	if (f==x)
@@ -50,23 +52,51 @@ void deplacment(float frequency) // dit au robot de tourner à droite, gauche ou 
 		}
 	if (rotation==0)
 		{
+			rotation=1;
 			if (f>x)
-				rotation=1;
 				{
 					if (diff=f-x>3)
 							{
-								//turn left 6-diff times
+								//turn left 6-diff steps
+								turn_left();
+								if (left_motor_get_pos<(-1)(1000/6)*(6-diff) & right_motor_get_pos>(1000/6)*(6-diff))
+									{
+										stop();
+										rotation=0;
+									}
 							}
-					else // turn right diff times
+					else
+							{
+								//turn right diff steps
+								turn_right();
+								if (right_motor_get_pos<(-1)(1000/6)*diff & left_motor_get_pos>(1000/6)*diff)
+									{
+										stop();
+										rotation=0;
+									}
+							}
 				}
 			if (f<x)
-				rotation=1;
 				{
 					if (diff=x-f>3)
 						{
-							//turn right 6-diff times
+							//turn right 6-diff steps
+							turn_right();
+							if (right_motor_get_pos<(-1)(1000/6)*diff & left_motor_get_pos>(1000/6)*diff)
+								{
+									stop();
+									rotation=0;
+								}
 						}
-					else // turn left diff times
+					else // turn left diff steps
+						{
+							turn_right();
+							if (right_motor_get_pos<(-1)(1000/6)*diff & left_motor_get_pos>(1000/6)*diff)
+								{
+									stop();
+									rotation=0;
+								}
+					}
 				}
 			x=f;
 		}
@@ -77,9 +107,18 @@ void go_straight(void)
 	left_motor_set_speed(600);
 	right_motor_set_speed(600);
 }
-void turn_right(void) // effectue 1 sixième de cercle
+void turn_right(void)
 {
-	
+	left_motor_set_speed(600);
+	right_motor_set_speed(-600);
 }
-
+void turn_left(void)
+	left_motor_set_speed(-600);
+	right_motor_set_speed(600);
+}
+void stop(void)
+{
+	left_motor_set_speed(0);
+	right_motor_set_speed(0);
+}
 
