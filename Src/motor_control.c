3d47794a7a90f5f247f443d54d8a974fe7bdd2bd
lucaspallez <lucas.pallez@epfl.ch>
2020-04-23 12:08:00 +0200

    Fixed errors and added default condition
    Added default stop condition and fixed compilation errors

--- Src/motor_control.c
+++ Src/motor_control.c
@@ -4,47 +4,48 @@
 
 
 
-void motor_control(float frequency) //test
-{
-	if(frequency >= 300 && frequency <= 400)
-		{
-				left_motor_set_speed(600);
-				right_motor_set_speed(600);
-		}
-		else if(frequency > 400 && frequency <= 500)
-		{
-			left_motor_set_speed(-600);
-			right_motor_set_speed(-600);
-		}
-		else
-		{
-			left_motor_set_speed(0);
-			right_motor_set_speed(0);
-		}
-}
+//void motor_control(float frequency) //test
+//{
+//	if(frequency >= 300 && frequency <= 400)
+//		{
+//				left_motor_set_speed(600);
+//				right_motor_set_speed(600);
+//		}
+//		else if(frequency > 400 && frequency <= 500)
+//		{
+//			left_motor_set_speed(-600);
+//			right_motor_set_speed(-600);
+//		}
+//		else
+//		{
+//			left_motor_set_speed(0);
+//			right_motor_set_speed(0);
+//		}
+//}
 
-void deplacement(float frequency) // dit au robot de tourner à droite, gauche ou de continuer tout
-// droit en fonction du son écouté.
+void motor_control(float frequency) // dit au robot de tourner a droite, gauche ou de continuer tout
+// droit en fonction du son ecoute.
 {
-	left_motor_set_pos(0);
-	right_motor_set_pos(0);
+//	left_motor_set_pos(0);
+//	right_motor_set_pos(0);
 	  //Violon:
 
 
 	int x=0; //position note du robot
-	int f=0; //position de la fréquence écoutée
+	int f=0; //position de la frequence ecoutee
 	int diff;
 	int rotation; //1 si rotation en cours, 0 sinon
 
-	//Codage de la frequence écoutée en fonction du schéma //Violon:
-	if (frequency>250 & frequency<280) f=0; //Do: 265.625
-	if (frequency>280 & frequency<312) f=1; //Ré: 296.875
-	if (frequency>312 & frequency<358) f=2; //Mi: 328.125
-	if (frequency>358 & frequency<390) f=3; //Fa: 375
-	if (frequency>390 & frequency<430) f=4; //Sol: 406.25
-	if (frequency>430 & frequency<480) f=5; //La: 453.125
+	//Codage de la frequence ecoutee en fonction du schema //Violon:
+	if (frequency>250 && frequency<280) f=0; //Do: 265.625
+	else if (frequency>280 && frequency<312) f=1; //Re: 296.875
+	else if (frequency>312 && frequency<358) f=2; //Mi: 328.125
+	else if (frequency>358 && frequency<390) f=3; //Fa: 375
+	else if (frequency>390 && frequency<430) f=4; //Sol: 406.25
+	else if (frequency>430 && frequency<480) f=5; //La: 453.125
+	else f=6;
 
-	// condition de déplacement
+	// condition de deplacement
 	if (f==x)
 		{
 			go_straight();
@@ -55,51 +56,62 @@ void deplacement(float frequency) // dit au robot de tourner à droite, gauche ou
 			rotation=1;
 			if (f>x)
 				{
-					if (diff=f-x>3)
+					if ((diff=f-x)>3)
 							{
 								//turn left 6-diff steps
 								turn_left();
-								if (left_motor_get_pos<(-1)(1000/6)*(6-diff) & right_motor_get_pos>(1000/6)*(6-diff))
+								if ((left_motor_get_pos()<(-1)*(1000/6)*(6-diff)) && (right_motor_get_pos()>(1000/6)*(6-diff)))
 									{
 										stop();
 										rotation=0;
+										left_motor_set_pos(0);
+										right_motor_set_pos(0);
 									}
 							}
 					else
 							{
 								//turn right diff steps
 								turn_right();
-								if (right_motor_get_pos<(-1)(1000/6)*diff & left_motor_get_pos>(1000/6)*diff)
+								if ((right_motor_get_pos()<(-1)*(1000/6)*diff) && (left_motor_get_pos()>(1000/6)*diff))
 									{
 										stop();
 										rotation=0;
+										left_motor_set_pos(0);
+										right_motor_set_pos(0);
 									}
 							}
 				}
-			if (f<x)
+			else if (f<x)
 				{
-					if (diff=x-f>3)
+					if ((diff = x-f) >3)
 						{
 							//turn right 6-diff steps
 							turn_right();
-							if (right_motor_get_pos<(-1)(1000/6)*diff & left_motor_get_pos>(1000/6)*diff)
+							if ((right_motor_get_pos()<(-1)*(1000/6)*diff) && (left_motor_get_pos()>(1000/6)*diff))
 								{
 									stop();
 									rotation=0;
+									left_motor_set_pos(0);
+									right_motor_set_pos(0);
 								}
 						}
 					else // turn left diff steps
 						{
 							turn_right();
-							if (right_motor_get_pos<(-1)(1000/6)*diff & left_motor_get_pos>(1000/6)*diff)
+							if ((right_motor_get_pos()<(-1)*(1000/6)*diff) && (left_motor_get_pos()>(1000/6)*diff))
 								{
 									stop();
 									rotation=0;
+									left_motor_set_pos(0);
+									right_motor_set_pos(0);
 								}
 					}
 				}
-			x=f;
+			x=f; //new position
 		}
+	if(f==6)
+		stop();
+
 }
 
 void go_straight(void)
@@ -113,6 +125,7 @@ void turn_right(void)
 	right_motor_set_speed(-600);
 }
 void turn_left(void)
+{
 	left_motor_set_speed(-600);
 	right_motor_set_speed(600);
 }
@@ -121,4 +134,3 @@ void stop(void)
 	left_motor_set_speed(0);
 	right_motor_set_speed(0);
 }
-
