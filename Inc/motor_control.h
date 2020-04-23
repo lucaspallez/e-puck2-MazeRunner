76ef804797322c334f4f95353263973d74714df6
lucaspallez <lucas.pallez@epfl.ch>
2020-04-23 12:09:37 +0200

    Added motor function definitions

--- Inc/motor_control.h
+++ Inc/motor_control.h
@@ -2,5 +2,9 @@
 #define INC_MOTOR_CONTROL_H_
 
 void motor_control(float frequency);
+void stop(void);
+void turn_left(void);
+void turn_right(void);
+void go_straight(void);
 
 #endif /* INC_MOTOR_CONTROL_H_ */
