bed70efdc89818e192dd4511009def1d4645fcbf
Achraf <achraf.berrada@epfl.ch>
2020-05-05 07:47:45 +0000

    test avec tout

--- Src/motor_control.c
+++ Src/motor_control.c
@@ -36,8 +36,8 @@ void motor_control(float frequency) // dit au robot de tourner a droite, gauche 
 
 	static int diff = 0;
 	static int rotation = 0;//1 si rotation en cours, 0 sinon
-	////balblabla
-	frequency= filtre(frequency);
+
+	//frequency= filtre(frequency);
 
 
 	//Codage de la frequence ecoutee en fonction du schema //Violon:
@@ -45,28 +45,98 @@ void motor_control(float frequency) // dit au robot de tourner a droite, gauche 
 	//on rend le robot sourd jusqu'à la fin de la rotation
     if (rotation==0)
     {
-//		if ((frequency>250) && (frequency<280)) f=0; //Do: 265.625
-//		else if ((frequency>280) && (frequency<312)) f=1; //Re: 296.875
-//		else if ((frequency>312) && (frequency<358)) f=2; //Mi: 328.125
-		if ((frequency>358) && (frequency<390)) f=3; //Fa: 375  kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk
-//		else if ((frequency>390) && (frequency<430)) f=4; //Sol: 406.25
-//		else if ((frequency>430) && (frequency<480)) f=5; //La: 453.125
+		if ((frequency>250) && (frequency<280)) f=0; //Do: 265.625
+		else if ((frequency>280) && (frequency<312)) f=1; //Re: 296.875
+		else if ((frequency>312) && (frequency<358)) f=2; //Mi: 328.125
+    	else if ((frequency>358) && (frequency<390)) f=3; //Fa: 375
+		else if ((frequency>390) && (frequency<430)) f=4; //Sol: 406.25
+		else if ((frequency>430) && (frequency<480)) f=5; //La: 453.125
 		else f=6;
     }
 
+//	if (f==x)
+//	{
+//		go_straight();
+//	}
+//
+//	if ((f>x) && (f!=6))
+//	{
+//		diff=f-x;
+//		{
+//			//turn left 6-diff steps
+//			turn_left();
+//			rotation=1;
+//			if ((left_motor_get_pos()<(-1)*((53/41)*1000/6)*(6-diff)) && (right_motor_get_pos()>((53/41)*1000/6)*(6-diff)))
+//			{
+//				stop();
+//				rotation=0;
+//				left_motor_set_pos(0);
+//				right_motor_set_pos(0);
+//				x=f; //new position
+//			}
+//		}
+//
+//	}
+
+	// condition de deplacement
 	if (f==x)
 	{
 		go_straight();
 	}
-
 	if ((f>x) && (f!=6))
 	{
 		diff=f-x;
+		if (diff>=3)
 		{
 			//turn left 6-diff steps
 			turn_left();
 			rotation=1;
-			if ((left_motor_get_pos()<(-1)*((53/41)*1000/6)*(6-diff)) && (right_motor_get_pos()>((53/41)*1000/6)*(6-diff)))
+			if ((left_motor_get_pos()<(-1)*((float)53/(float)41)*((float)1000/(float)6)*(6-diff)) && (right_motor_get_pos()>((float)53/(float)41)*((float)1000/(float)6)*(6-diff)))
+			{
+				stop();
+				rotation=0;
+				left_motor_set_pos(0);
+				right_motor_set_pos(0);
+				x=f; //new position
+			}
+		}
+		else
+		{
+			//turn right diff steps
+			turn_right();
+			rotation=1;
+			if ((right_motor_get_pos()<(-1)*((float)53/(float)41)*((float)1000/(float)6)*diff) && (left_motor_get_pos()>((float)53/(float)41)*((float)1000/(float)6)*diff))
+			{
+				stop();
+				rotation=0;
+				left_motor_set_pos(0);
+				right_motor_set_pos(0);
+				x=f; //new position
+			}
+		}
+	}
+	if ((f<x)&& (f!=6))
+	{
+		diff = x-f;
+		if (diff>=3)
+		{
+			//turn right 6-diff steps
+			turn_right();
+			rotation=1;
+			if ((right_motor_get_pos()<((-1)*((float)53/(float)41)*((float)1000/(float)6)*(6-diff))) && (left_motor_get_pos()>(((float)53/(float)41)*((float)1000/(float)6)*(6-diff))))
+			{
+				stop();
+				rotation=0;
+				left_motor_set_pos(0);
+				right_motor_set_pos(0);
+				x=f; //new position
+			}
+		}
+		else // turn left diff steps
+		{
+			turn_left();
+			rotation=1;
+			if ((left_motor_get_pos()<(-1)*((float)53/(float)41)*((float)1000/(float)6)*diff) && (right_motor_get_pos()>((float)1000/(float)6)*((float)53/(float)41)*diff))
 			{
 				stop();
 				rotation=0;
@@ -77,78 +147,13 @@ void motor_control(float frequency) // dit au robot de tourner a droite, gauche 
 		}
 	}
 
-
-
-
-	// condition de deplacement
-//	if (f==x)
-//	{
-//		go_straight();
-//	}
-//	if (f>x)
-//	{
-//		if ((diff=f-x)>=3)
-//		{
-//			//turn left 6-diff steps
-//			turn_left();
-//			rotation=1;
-//			if ((left_motor_get_pos()<(-1)*(1000/6)*(6-diff)) && (right_motor_get_pos()>(1000/6)*(6-diff)))
-//			{
-//				stop();
-//				rotation=0;
-//				left_motor_set_pos(0);
-//				right_motor_set_pos(0);
-//				x=f; //new position
-//			}
-//		}
-//		else
-//		{
-//			//turn right diff steps
-//			turn_right();
-//			rotation=1;
-//			if ((right_motor_get_pos()<(-1)*(1000/6)*diff) && (left_motor_get_pos()>(1000/6)*diff))
-//			{
-//				stop();
-//				rotation=0;
-//				left_motor_set_pos(0);
-//				right_motor_set_pos(0);
-//				x=f; //new position
-//			}
-//		}
-//	}
-//	if (f<x)
-//	{
-//		if ((diff = x-f)>=3)
-//		{
-//			//turn right 6-diff steps
-//			turn_right();
-//			rotation=1;
-//			if ((right_motor_get_pos()<(-1)*(1000/6)*(6-diff) && (left_motor_get_pos()>((1000/6)*(6-diff)))
-//			{
-//				stop();
-//				rotation=0;
-//				left_motor_set_pos(0);
-//				right_motor_set_pos(0);
-//				x=f; //new position
-//			}
-//		}
-//		else // turn left diff steps
-//		{
-//			turn_left();
-//			rotation=1;
-//			if ((left_motor_get_pos()<(-1)*(1000/6)*diff) && (_motor_get_pos()>(1000/6)*diff))
-//			{
-//				stop();
-//				rotation=0;
-//				left_motor_set_pos(0);
-//				right_motor_set_pos(0);
-//				x=f; //new position
-//			}
-//		}
-//	}
-
 	if((f==6)) stop();
 }
+
+//if ((left_motor_get_pos()<(-1)*((((float) 53)/ (float) 41)* (float) 1000/ (float) 6)*(6-diff)) &&
+				//(right_motor_get_pos()>((((float) 53)/ (float) 41)* (float) 1000/ (float) 6)*(6-diff)))
+
+
 
 
 void go_straight(void)
@@ -158,32 +163,32 @@ void go_straight(void)
 }
 void turn_right(void)
 {
-	left_motor_set_speed(1000);
-	right_motor_set_speed(-1000);
+	left_motor_set_speed(600);
+	right_motor_set_speed(-600);
 }
 void turn_left(void)
 {
-	left_motor_set_speed(-1000);
-	right_motor_set_speed(1000);
+	left_motor_set_speed(-600);
+	right_motor_set_speed(600);
 }
 void stop(void)
 {
 	left_motor_set_speed(0);
 	right_motor_set_speed(0);
 }
-int filtre(int frequency)
-{
-	static int tableau[5] = {0};
-	static int i=0;
-	int j;
-	int moyenne;
-	tableau[i]=frequency;
-	i++;
-	if (i==4) i=0;
-	for (j=0; j<=4 ;j++) moyenne=+tableau[j];
-	moyenne=moyenne/5;
-	return moyenne;
-
-}
-
+//int filtre(int frequency)
+//{
+//	static int tableau[5] = {0};
+//	static int i=0;
+//	int j;
+//	int moyenne;
+//	tableau[i]=frequency;
+//	i++;
+//	if (i==4) i=0;
+//	for (j=0; j<=4 ;j++) moyenne=+tableau[j];
+//	moyenne=moyenne/5;
+//	return moyenne;
+//
+//}
+//
 
