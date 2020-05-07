3b9dd8d89c897d691b6eb94c73d954f9539e13b2
Lucas Pallez <lucas.pallez@hotmail.com>
2020-05-07 12:31:41 +0200

    Fixed motor behavior
    Motors behave correctly on frequency input from mic now.
    Added motor_pos reset before each rotation and added condition that a new frequency must be detected to rotate again.
    Also cleaned up the code further.

--- Src/motor_control.c
+++ Src/motor_control.c
@@ -5,39 +5,28 @@
 void motor_control(float frequency) // dit au robot de tourner a droite, gauche ou de continuer tout
 // droit en fonction du son ecoute.
 {
-//	left_motor_set_pos(0);
-//	right_motor_set_pos(0);
-	static int x=0; //position note du robot
-	static int f=6; //position de la frequence ecoutee changement intialisation
-	static int diff = 0;
-	static int rotation = 0;//1 si rotation en cours, 0 sinon
-	static systime_t t_temp = 0;
+	static uint8_t x=0; //position note du robot
+	static uint8_t f=6; //position de la frequence ecoutee changement intialisation
+	static uint8_t diff = 0;
+	static uint8_t rotation = 0;//1 si rotation en cours, 0 sinon
+	static uint16_t freq_temp = 0;
 
-	//frequency= filtre(frequency);
 
 	//Codage de la frequence ecoutee en fonction du schema
 	// condition nécessaire pour éviter changement de comportement avant la fin d'une rotation,
 	//on rend le robot sourd jusqu'à la fin de la rotation
-//	if (rotation==0)
-//	{
-//		if ((frequency>500) && (frequency<540)) f=0; //Do: 523
-//		else if ((frequency>560) && (frequency<610)) f=1; //Re: 587
-//		else if ((frequency>630) && (frequency<670)) f=2; //Mi: 659
-//    	else if ((frequency>675) && (frequency<720)) f=3; //Fa: 698
-//		else if ((frequency>760) && (frequency<810)) f=4; //Sol:783
-//		else if ((frequency>850) && (frequency<900)) f=5; //La: 880
-//		else f=6;
-//	}
-	t_temp = chVTGetSystemTime();
-	if (rotation==0)
+	if (rotation==0 && (freq_temp != frequency))
 	{
-		if ((t_temp) > MS2ST(2000)) f=0; //Do: 523
-		if ((t_temp) > MS2ST(4000)) f=1; //Re: 587
-		if ((t_temp) > MS2ST(6000)) f=2; //Mi: 659
-		if ((t_temp) > MS2ST(8000)) f=3; //Fa: 698
-		if ((t_temp) > MS2ST(10000)) f=4; //Sol:783
-		if ((t_temp) > MS2ST(12000)) f=5; //La: 880
-		if ((t_temp) > MS2ST(14000)) f=6;
+		freq_temp = frequency;
+		left_motor_set_pos(0);
+		right_motor_set_pos(0);
+		if ((frequency>500) && (frequency<540)) f=0; //Do: 523
+		else if ((frequency>560) && (frequency<610)) f=1; //Re: 587
+		else if ((frequency>630) && (frequency<670)) f=2; //Mi: 659
+    	else if ((frequency>675) && (frequency<720)) f=3; //Fa: 698
+		else if ((frequency>760) && (frequency<810)) f=4; //Sol:783
+		else if ((frequency>850) && (frequency<900)) f=5; //La: 880
+		else f=6;
 	}
 	// condition de deplacement
 	if(f==x)
